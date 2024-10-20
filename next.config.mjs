3c9bb178c3f3bd19a4cba4dcd7ef1d32556a1f7b
keampichvireak <82803740+keampichvireak@users.noreply.github.com>
2024-10-20 11:26:17 +0700

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "plus.unsplash.com"], // Add any other trusted domains here
+  },
+};
+
+export default nextConfig;
